9da74111416299c567125a342f784f9b65b00e6a
Kevin <Kevin.FNCW@gmail.com>
2021-05-10 22:20:24 -0400

    add campus and student schemas

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -1,17 +1,33 @@
 const Sequelize = require('sequelize');
+const { validate } = require('../db');
 const db = require('../db');
 
-const Campus = db.define("campus", {
+const Campus = db.define('campus', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
 
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue:
+            'https://www.flaticon.com/svg/vstatic/svg/3315/3315761.svg?token=exp=1620698998~hmac=c963694631776a276112d071b52136b2',
+    },
 
-  description: {
-    type: Sequelize.STRING,
-  }
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
 
+    description: {
+        type: Sequelize.TEXT,
+    },
 });
 
 module.exports = Campus;
--- database/models/Student.js
+++ database/models/Student.js
@@ -1,18 +1,46 @@
 const Sequelize = require('sequelize');
+const { validate } = require('../db');
 const db = require('../db');
 
-const Student = db.define("student", {
+const Student = db.define('student', {
+    firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: true,
+        },
+    },
 
-  firstname: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+    lastname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: true,
+        },
+    },
 
-  lastname: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        },
+    },
 
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue:
+            'https://www.flaticon.com/svg/vstatic/svg/194/194931.svg?token=exp=1620699950~hmac=913a21c60bee24ef5fe276b69859ecff',
+    },
+
+    gpa: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+        validate: {
+            min: 0.0,
+            max: 4.0,
+        },
+    },
 });
 
 module.exports = Student;
