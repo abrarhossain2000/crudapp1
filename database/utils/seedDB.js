c4556e278abb9bb965df6b893c93f415fd317542
Kevin <Kevin.FNCW@gmail.com>
2021-05-13 17:42:19 -0400

    add properties for updated schemas to seed

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -1,22 +1,25 @@
 const { Campus, Student } = require('../models');
 
 const seedDB = async () => {
-	const dummy_campus = await Campus.create({
-		name: "Hunter College",
-		description: "This is a school in NYC."
-	});
-	const dummy_campus2 = await Campus.create({
-		name: "Harvard",
-		description: "This is a school in MA."
-	});
+    const dummy_campus = await Campus.create({
+        name: 'Hunter College',
+        description: 'This is a school in NYC.',
+        address: '695 Park Ave, New York, NY 10065',
+    });
+    const dummy_campus2 = await Campus.create({
+        name: 'Harvard',
+        description: 'This is a school in MA.',
+        address: '1465-1483 Massachusetts Ave, Cambridge, MA 02138',
+    });
 
-	const dummy_student = await Student.create({
-			firstname: "Joe",
-      lastname: "Shmo",
-		});
+    const dummy_student = await Student.create({
+        firstname: 'Joe',
+        lastname: 'Shmo',
+        email: 'Joe.Shmo@gmail.com',
+        gpa: 3.28,
+    });
 
-	await dummy_student.setCampus(dummy_campus);
-	
-}
+    await dummy_student.setCampus(dummy_campus);
+};
 
 module.exports = seedDB;
